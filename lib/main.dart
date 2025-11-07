8708f9e6048a77625f53128e958bde13fbc26e8b
brian.horacio13 <brian.horacio13@gmail.com>
2025-11-07 05:55:29 +0000

    Implementacion de Firebase para la base de datos y su capacidad de Login y forms.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'nuevoreporte.dart';
 import 'misreportes.dart';
 import 'micuenta.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
